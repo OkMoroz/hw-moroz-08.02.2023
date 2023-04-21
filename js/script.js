147e486a6ec89f21ac4392f941ab662133bf8cac
Oksana Moroz <k.moroz.mail@gmail.com>
2023-04-21 11:56:28 +0300

    new pages

--- js/script.js
+++ js/script.js
@@ -3,3 +3,19 @@
 //     $(this).toggleClass("active")
 //     $("header-link").slideToggle()
 // })
+
+$("#slider-items").owlCarousel({
+  loop: true,
+  nav: false,
+  dots: true,
+  margin: 40,
+  items: 4,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    760: {
+      items: 4,
+    },
+  },
+});
